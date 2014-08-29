3c78e796872c24a98485bedf28b7f920c3f221d2
Stephen Henderson <s.p.henderson@gmail.com>
2014-08-29 17:54:10 +0100

    Tidying and comments

--- ipforcity.py
+++ ipforcity.py
@@ -4,20 +4,31 @@ __author__ = 'stephenhenderson'
 
 from collections import namedtuple
 
-
-def make_city_key(city_name, country_code):
-        return "{}:{}".format(country_code, city_name).lower()
-
 class CityToIpMap(namedtuple('CityToIpMap', 'city_to_ip')):
 
     def get_ip_for_city(self, city, country_code):
         city_key = make_city_key(city_name=city, country_code=country_code)
         return self.city_to_ip.get(city_key, None)
 
+def make_city_key(city_name, country_code):
+    return "{}:{}".format(country_code, city_name).lower()
+
 
 def build_city_to_ip_map(city_file, ip_file):
+    """
+    Parses the maxmind geo-ip datasets and generates a dict of city-key (see
+    make_city_key()) to ipv4 address.
+
+    :param city_file: maxmind geo-ip city-locations csv
+    :param ip_file: maxmind geo-ip city-blocks csv
+    :return: dict of city-key to ipaddress
+    """
+
     city_to_ip_address = dict()
     city_to_ip_range = build_city_to_ip_range_id_map(city_file)
+
+    # See schema for ip-file here:
+    # http://dev.maxmind.com/geoip/geoip2/geoip2-csv-databases/#Blocks_File
     ip_v4_prefix = "::ffff:"
     ip_v4_prefix_len = len(ip_v4_prefix)
 
@@ -38,8 +49,12 @@ def build_city_to_ip_range_id_map(city_file):
     print("Extracting ip-ranges for cities from {}".format(city_file.name))
     city_to_ip_range = dict()
     first_line = True
+
+    # See schema for city locations file here:
+    # http://dev.maxmind.com/geoip/geoip2/geoip2-csv-databases/#Location_File
     for line in city_file:
         if first_line:
+            # Skip csv header
             first_line = False
             continue
         fields = line.split(",")
